7da69605f8cbf476ac1395f476f7d05d1851d649
aidancjones <97634617+aidancjones@users.noreply.github.com>
2022-03-29 10:41:52 -0600

    java

---
+++ js/note.js
@@ -0,0 +1,61 @@
+/* let myVariable;
+myVariable = 'Aidan';
+
+let myVariable2 = 'Peter';
+
+myVariable = 'Bob';
+
+/* This is a comment
+multi-line comment
+*/
+
+// This is a comment
+
+let myVariable = 'bob'; //STRINGS
+
+let myNumber = 3 // NUMBERS
+
+let myBoolean = true; //BOOLEANS
+let myBoolean = false;
+
+let myArray = [1, 'bob', true]; //ARRAY
+
+let myObject = document.querySelector('p');
+//OBJECTS, BUT ALL THE ABOVE ARE OBJECTS // TODO:
+
+//OPERATORS
+
+6 + 9; //ADDITION
+'Hello' + 'World' //STRING ADDITION --> 'HelloWorld'
+
+
+//SUBTRACTION
+//MULTIPLICATION --> *
+//DIVISION --> /
+
+//ASSIGNMENT -- =
+
+//EQUALITY
+myNumber === 4; // <--This will result in false
+
+//NOT, DOES-NOT-EQUAL
+!(myNumber === 3); // !(TRUE) = FALSE;
+myVariable ! == 3; //FALSE
+
+
+//CONDITIONALS
+
+let iceCream = 'chocolate';
+if(iceCream === 'chocolate')' {
+  alert('Yay, I love chocolate ice cream!');
+} else {
+  alert('Awww, but chocolate is my favorite...');
+}
+
+
+// EVENT LISTENERS
+
+document.querySelector('html').addEventListener('click',
+function() {
+  alert('Ouich! Stop poking me!');
+});
